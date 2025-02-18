1eff0f6a6e8cd0efb9a1baf1f21c99afcaa6d4a8
heyuanjie87 <943313837@qq.com>
2025-02-18 15:27:52 +0800

    修改配置

--- driver/board.h
+++ driver/board.h
@@ -21,9 +21,9 @@ extern unsigned int __bss_end;
 #endif
 
 #define RT_HW_HEAP_BEGIN ((void *)&__bss_end)
-#define RT_HW_HEAP_END   ((void *)(RT_HW_HEAP_BEGIN + 64 * 1024 * 1024))
+#define RT_HW_HEAP_END   ((void *)(RT_HW_HEAP_BEGIN + 8 * 1024 * 1024))
 #define RT_HW_PAGE_START RT_HW_HEAP_END
-#define RT_HW_PAGE_END   ((void *)(KERNEL_VADDR_START + (256 * 1024 * 1024 - 0x200000)))
+#define RT_HW_PAGE_END   ((void *)(KERNEL_VADDR_START + (66 * 1024 * 1024 - 0x200000)))
 
 void rt_hw_board_init(void);
 void rt_init_user_mem(struct rt_thread *thread, const char *name,
